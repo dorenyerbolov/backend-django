2246a1b3bb21e50c8acb9a59f981540c76b2cd4c
Dauren <doren.yerbolov@gmail.com>
2021-02-24 00:58:58 +0600

    lab3 done

--- todo/main/views.py
+++ todo/main/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
 
-# Create your views here.
+todos = [
+    {
+        'title': 'Task 0',
+        'created': '10/09/2018',
+        'due_on': '12/09/2018',
+        'owner': 'admin',
+        'done': False
+    },
+    {
+        'title': 'Task 1',
+        'created': '10/09/2018',
+        'due_on': '12/09/2018',
+        'owner': 'admin',
+        'done': True
+    },
+    {
+        'title': 'Task 2',
+        'created': '10/09/2018',
+        'due_on': '12/09/2018',
+        'owner': 'admin',
+        'done': True
+    },
+    {
+        'title': 'Task 3',
+        'created': '10/09/2018',
+        'due_on': '12/09/2018',
+        'owner': 'admin',
+        'done': True
+    },
+    {
+        'title': 'Task 4',
+        'created': '10/09/2018',
+        'due_on': '12/09/2018',
+        'owner': 'admin',
+        'done': True
+    },
+]
+
+
+def todo_list(request):
+    context = {
+        'todos': filter(lambda task: True if task['done'] else False, todos)
+    }
+    return render(request, 'todo_list.html', context=context)
+
+
+def completed_todo_list(request):
+    context = {
+        'todos': filter(lambda task: True if not task['done'] else False, todos)
+    }
+    return render(request, 'completed_todo_list.html', context=context)
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '^u536yxmne_gn1@ids5^(ku8qiy8ym-lr2f@k0)y!*=)(&gsi2'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -56,7 +54,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'todo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -115,8 +110,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# this one is used when you want django to look for static files not only inside app dirs
+# STATICFILES_DIRS = (
+#     (BASE_DIR / 'static'),
+# )
+
+STATIC_ROOT = (BASE_DIR / 'staticfiles')
